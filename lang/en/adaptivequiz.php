ce3ade8182871ca59d7badcb8fd8ac6611f4012f
Luca Gladiator <lucamarius.gladiator@stud.tu-darmstadt.de>
2017-11-21 11:58:14 +0100

    initial commit

---
+++ lang/en/adaptivequiz.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for adaptivequiz
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_adaptivequiz
+ * @copyright  2016 Your Name <your@email.address>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Adaptive Quiz';
+$string['modulenameplural'] = 'Adaptive Quizes';
+$string['modulename_help'] = 'Use the adaptivequiz module for... | The adaptivequiz module allows...';
+$string['adaptivequiz:addinstance'] = 'Add a new Adaptive Quiz';
+$string['adaptivequiz:submit'] = 'Submit Adaptive Quiz';
+$string['adaptivequiz:view'] = 'View Adaptive Quiz';
+$string['adaptivequizfieldset'] = 'Custom example fieldset';
+$string['adaptivequizname'] = 'Adaptive Quiz name';
+$string['adaptivequizname_help'] = 'This is the content of the help tooltip associated with the adaptivequizname field. Markdown syntax is supported.';
+$string['adaptivequiz'] = 'Adaptive Quiz';
+$string['pluginadministration'] = 'Adaptive Quiz administration';
+$string['pluginname'] = 'Adaptive Quiz';
