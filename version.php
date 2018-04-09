6bc3ce491b282a46d73b7796addeea0298eb71a4
Luca Gladiator <lucamarius.gladiator@stud.tu-darmstadt.de>
2018-04-09 12:10:12 +0200

    Version increase for logo

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_ddtaquiz';
-$plugin->version = 2018032800;
+$plugin->version = 2018040900;
 $plugin->release = 'v0.0';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
